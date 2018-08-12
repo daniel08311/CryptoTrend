d1a80953c356d3c3c3b82638ca68f95b4e438e57
daniel08311 <b03705013@ntu.edu.tw>
2018-08-12 19:02:46 +0800

    ui update

--- run.py
+++ run.py
@@ -54,9 +54,11 @@ else:
         with open('model_list.json','w') as data_file:    
             lists[args.exchange + "_" + args.name] = True
             json.dump(lists, data_file)
-    print("\n################################################################")
-    print("\nTraining on {} data with {} threads, Saving model as {}\n".format(args.exchange, args.thread, args.name))
-    print("################################################################\n")
+    print("\n##########################################")
+    print("\nTraining on {} data with {} threads".format(args.exchange, args.thread))
+    print("\nShiftx : {}, Shifty : {}".format(args.shiftx, args.shifty))
+    print("\nSaving model as {}".format(args.name))
+    print("\n##########################################\n")
     trend = trend.CryptoTrend(args.exchange, args.shiftx, args.shifty, args.name, args.thread)
     trend.get()
     trend.parse()
