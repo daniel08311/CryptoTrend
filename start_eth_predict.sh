4ee9e66b9d431ecee017bfaf3bee7348ba5a9dfa
daniel08311 <b03705013@ntu.edu.tw>
2018-08-13 17:37:47 +0800

    prediction cooldown reduced

--- start_eth_predict.sh
+++ start_eth_predict.sh
@@ -13,7 +13,7 @@ while true
     python3 run.py -exchange huobipro -coin ETH -name 1h -shiftx 200 -shifty 100 -thread 4 -test
     python3 run.py -exchange huobipro -coin ETH -name 30m -shiftx 100 -shifty 50 -thread 4 -test
 
-    secs=$((10 * 60))
+    secs=$((1 * 10))
     while [ $secs -gt 0 ]; do
         echo -ne "Cooldown for next round of ETH training in $secs\033[0K\r"
         sleep 1
